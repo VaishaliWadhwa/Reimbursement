8a202f7221a27d06ce52080cf652301615f4c2c7
Hritik Chauhan <hrithikc28@gmail.com>
2018-06-26 17:00:19 +0630

    Add ExpenseReport

---
+++ app/src/main/java/com/example/abc/reimbursement/ExpenseReport.java
@@ -0,0 +1,13 @@
+package com.example.abc.reimbursement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExpenseReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense_report);
+    }
+}
