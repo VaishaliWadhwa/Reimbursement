ca1643e5a4c99f0777e6012ce25fb9ad4c5476fe
Hritik Chauhan <hrithikc28@gmail.com>
2018-06-26 17:37:52 +0630

    Set OnClickListener on Floating Button in ExpenseReport

--- app/src/main/java/com/example/abc/reimbursement/ExpenseReport.java
+++ app/src/main/java/com/example/abc/reimbursement/ExpenseReport.java
@@ -1,13 +1,39 @@
 package com.example.abc.reimbursement;
 
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class ExpenseReport extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense_report);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ExpenseReport.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
 }
