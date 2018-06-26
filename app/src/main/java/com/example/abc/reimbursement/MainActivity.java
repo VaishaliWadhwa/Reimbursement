0ac2eabbb0e782cd8acb1e9239ff4b77d9a9918f
VAISHALI <wadhwavaishali2015@gmail.com>
2018-06-26 05:07:08 -0700

    Main Activity Updated

--- app/src/main/java/com/example/abc/reimbursement/MainActivity.java
+++ app/src/main/java/com/example/abc/reimbursement/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.abc.reimbursement;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Displays list of pets that were entered and stored in the app.
  */
 public class MainActivity extends AppCompatActivity {
 
-    /**
+    /*
      * Database helper that will provide us access to the database
      */
 
@@ -20,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
 
         // Setup FAB to open EditorActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ExpenseReport.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        // mDbHelper = new PetDbHelper(this);
+
+
+        // displayDatabaseInfo();
     }
 
 }
