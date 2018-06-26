bb32d4ea8ea3cd3e9bab93ab6bb033176dafe8a3
VAISHALI <wadhwavaishali2015@gmail.com>
2018-06-26 04:49:59 -0700

    Merge branch 'master' of C:\Users\ABC\Downloads\Reimbursement with conflicts.

--- app/src/main/java/com/example/abc/reimbursement/Main2Activity.java
+++ app/src/main/java/com/example/abc/reimbursement/Main2Activity.java
@@ -1,13 +1,90 @@
 package com.example.abc.reimbursement;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
+        Spinner mySpinner = (Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Main2Activity.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+        mySpinner.setOnItemSelectedListener(this);
+
+    }
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int i, long id) {
+        switch(i)
+        {
+            case (1) :
+            {
+
+
+                Intent intent = new Intent (Main2Activity.this , Pop.class );
+                startActivity(intent);
+                break;
+            }
+
+            case (2):
+            {
+
+                Intent intent = new Intent (Main2Activity.this , Travel.class );
+                startActivity(intent);
+                break;
+
+
+
+
+            }
+            case (4):
+            {
+
+                Intent intent = new Intent (Main2Activity.this , TeamExpenseActivity.class );
+                startActivity(intent);
+                break;
+
+
+
+
+            }
+            case (5):
+            {
+
+                Intent intent = new Intent (Main2Activity.this , MiscellaneousExpenseActivity.class );
+                startActivity(intent);
+                break;
+
+
+
+
+            }
+            default :
+            {
+                Toast.makeText(this, "Slct other", Toast.LENGTH_SHORT).show();
+                break;
+
+            }
+
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/com/example/abc/reimbursement/MainActivity.java
+++ app/src/main/java/com/example/abc/reimbursement/MainActivity.java
@@ -1,93 +1,25 @@
 package com.example.abc.reimbursement;
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
 
-import android.content.Intent;
+/**
+ * Displays list of pets that were entered and stored in the app.
+ */
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    /**
+     * Database helper that will provide us access to the database
+     */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner mySpinner = (Spinner)findViewById(R.id.spinner1);
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.names));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner.setAdapter(myAdapter);
-        mySpinner.setOnItemSelectedListener(this);
 
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     }
 
-
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int i, long id) {
-        switch(i)
-        {
-            case (1) :
-            {
-
-
-                Intent intent = new Intent (MainActivity.this , Pop.class );
-                startActivity(intent);
-                break;
-            }
-
-            case (2):
-            {
-
-                Intent intent = new Intent (MainActivity.this , Travel.class );
-                startActivity(intent);
-                break;
-
-
-
-
-            }
-            case (4):
-            {
-
-                Intent intent = new Intent (MainActivity.this , TeamExpenseActivity.class );
-                startActivity(intent);
-                break;
-
-
-
-
-            }
-            case (5):
-            {
-
-                Intent intent = new Intent (MainActivity.this , MiscellaneousExpenseActivity.class );
-                startActivity(intent);
-                break;
-
-
-
-
-            }
-            default :
-            {
-                Toast.makeText(this, "Slct other", Toast.LENGTH_SHORT).show();
-                break;
-
-            }
-
-        }
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 }
-
