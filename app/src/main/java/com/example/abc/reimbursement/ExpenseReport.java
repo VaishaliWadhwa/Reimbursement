3b4e1ab36d103ae7d63c662de716dd5141efd282
Hritik Chauhan <hrithikc28@gmail.com>
2018-06-30 15:34:36 +0630

    Before database handling

--- app/src/main/java/com/example/abc/reimbursement/ExpenseReport.java
+++ app/src/main/java/com/example/abc/reimbursement/ExpenseReport.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 public class ExpenseReport extends AppCompatActivity {
@@ -14,6 +15,13 @@ public class ExpenseReport extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense_report);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int) (width * .70), (int) (height * .70));
 
         // Setup FAB to open EditorActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
