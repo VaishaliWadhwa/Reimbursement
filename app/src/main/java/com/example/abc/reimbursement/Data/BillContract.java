f74b4ddd75b5175db90646052174f3111231cd0e
Hritik Chauhan <hrithikc28@gmail.com>
2018-06-30 19:31:20 +0630

    Edited BillCursorAdapter

--- app/src/main/java/com/example/abc/reimbursement/Data/BillContract.java
+++ app/src/main/java/com/example/abc/reimbursement/Data/BillContract.java
@@ -31,7 +31,7 @@ public class BillContract {
      * looking at pet data. content://com.example.android.pets/staff/ will fail,
      * as the ContentProvider hasn't been given any information on what to do with "staff".
      */
-    public static final String PATH_PETS = "";
+    public static final String PATH_EXPENSES = "Expenses";
 
     /**
      * Inner class that defines constant values for the pets database table.
@@ -43,18 +43,18 @@ public class BillContract {
          * The MIME type of the {@link #CONTENT_URI} for a list of pets.
          */
         public static final String CONTENT_LIST_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_EXPENSES;
 
         /**
          * The MIME type of the {@link #CONTENT_URI} for a single pet.
          */
         public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_EXPENSES;
 
         /**
          * The content URI to access the pet data in the provider
          */
-        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_EXPENSES);
 
         /**
          * Name of database table for pets
@@ -95,9 +95,6 @@ public class BillContract {
         public final static String COLUMN_EXPENSE_CAT = "category";
 
         public final static String COLUMN_EXPENSE_SUBCAT = "sub_category";
-        public final static String COLUMN_EXPENSE_BILL_ID= "Bill_ID";
-
-
 
     }
 }
